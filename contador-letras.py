bf48537a9be5709a994da04119cd2f1fdf9230df
naranda <natya518.redes@gmail.com>
2025-09-14 11:08:21 -0500

    Contador de letras de palabras

---
+++ contador-letras.py
@@ -0,0 +1,13 @@
+# Paso 1: solicitar entrada al usuario
+
+palabra_ingresada = input("Por favor, ingrese la palabra: ")
+
+
+# Paso 2: Contar la cantidad de letras
+
+cant_letras = len(palabra_ingresada)
+
+
+#Paso 3: Mostrar al usuario el resultado
+
+print("La palabra ingresada tiene", cant_letras, "letras.")
